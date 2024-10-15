818b0bea49b69784439e12bd4695a744f7c64330
zeref80 <12345qaz6789@wp.pl>
2024-10-15 16:37:20 +0200

    cert

--- pdo.php
+++ pdo.php
@@ -1,4 +1,5 @@
 <?php
-    $pdo = new PDO('mysql:host=chatapp2.mysql.database.azure.com;port=3306;dbname=chat_db', 'fgoccwpufa', 'Chatappdb1');
+	$cert = 'DigiCertGlobalRootCA.crt.pem';
+    $pdo = new PDO('mysql:host=chatapp2.mysql.database.azure.com;port=3306;dbname=chat_db', 'fgoccwpufa', 'Chatappdb1', $cert);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
