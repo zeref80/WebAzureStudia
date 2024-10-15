09995690e1556c5a813344d2a2fd4e321d14131d
zeref80 <45498824+zeref80@users.noreply.github.com>
2024-10-15 16:45:58 +0200

    Update pdo.php - v2

--- pdo.php
+++ pdo.php
@@ -2,7 +2,7 @@
     $cert = 'DigiCertGlobalRootCA.crt.pem';
     $options = [
         PDO::MYSQL_ATTR_SSL_CA => $cert,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     ];
     $pdo = new PDO('mysql:host=chatapp2.mysql.database.azure.com;port=3306;dbname=chat_db', 'fgoccwpufa', 'Chatappdb1', $options);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
