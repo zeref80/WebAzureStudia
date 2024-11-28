6c7ad3ed5686846dd353b4dbcb6b986785d30b9f
Jakub Dąbrowski <62817391+oeska13@users.noreply.github.com>
2024-11-28 12:59:13 +0100

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,8 +1,8 @@
 <?php
-    $cert = 'DigiCertGlobalRootCA.crt.pem';
-    $options = [
-        PDO::MYSQL_ATTR_SSL_CA => $cert,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    ];
-    $pdo = new PDO('mysql:host=chatapp-db.mysql.database.azure.com;port=3306;dbname=chat_db', 'adm_cht54', 'QCG77aqGLyVrYPM', $options);
+    //$cert = 'DigiCertGlobalRootCA.crt.pem';
+   // $options = [
+        //PDO::MYSQL_ATTR_SSL_CA => $cert,
+        //PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+   // ];
+    $pdo = new PDO('mysql:host=chatapp-db.mysql.database.azure.com;port=3306;dbname=chat_db', 'adm_cht54', 'QCG77aqGLyVrYPM');
 ?>
